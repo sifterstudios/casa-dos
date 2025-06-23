1de95840952662efb36e3e0a71cdf38b9d600af4
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2025-06-23 23:23:04 +0200

    feat: make netlify happy again

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,10 +14,7 @@ const config: Config = {
         v4: true // Improve compatibility with the upcoming Docusaurus v4
     },
 
-    // Set the production url of your site here
     url: 'https://casa.sifterstudios.no',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
 
     // GitHub pages deployment config.
